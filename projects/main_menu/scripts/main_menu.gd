a18d22f770b14bd2175a6da7c56d15908b970a2b
Pablo <pablo_penna7@hotmail.com>
2023-12-09 21:10:31 +0100

    New menu scene that allows to load the other ones

---
+++ projects/main_menu/scripts/main_menu.gd
@@ -0,0 +1,24 @@
+extends Node
+@export var window_size: Vector2 = Vector2(800, 600)
+@export var camera_scene: PackedScene
+@export var state_machine_scene: PackedScene
+@export var path_finding_scene: PackedScene
+@export var player_scene: PackedScene
+
+func _ready():
+	_set_screen_size(window_size)
+	
+func _set_screen_size(size: Vector2):
+	get_viewport().get_window().size = size
+
+func _on_camera_pressed():
+	get_tree().change_scene_to_packed(camera_scene)
+
+func _on_state_machine_pressed():
+	get_tree().change_scene_to_packed(state_machine_scene)
+
+func _on_path_finding_pressed():
+	get_tree().change_scene_to_packed(path_finding_scene)
+
+func _on_player_pressed():
+	get_tree().change_scene_to_packed(player_scene)
