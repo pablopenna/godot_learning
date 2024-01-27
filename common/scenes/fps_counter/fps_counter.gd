26c64fd58216ba292e6a89f3c554298b7e7a3268
Pablo <pablo_penna7@hotmail.com>
2024-01-27 03:20:52 +0000

    Fix FPS counter to always be in a fixed position in the window even if the camera moves

--- common/scenes/fps_counter/fps_counter.gd
+++ common/scenes/fps_counter/fps_counter.gd
@@ -4,4 +4,12 @@ func _ready():
 	position = get_viewport_rect().position
 
 func _process(delta):
+	_fix_position()
 	text = "%s" % Engine.get_frames_per_second()
+	
+func _fix_position():
+	var cam_position = get_viewport().get_camera_2d().position
+	var window_size = get_viewport().get_window().size
+	var top_left = Vector2(cam_position.x - window_size.x/2, cam_position.y - window_size.y/2)
+	position = top_left
+	
